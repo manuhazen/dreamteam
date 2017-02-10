3c5e167bf20f68ca879e8e91cef943013049b99c
Emmanuel Jimenez <manuhazen8@outlook.com>
2017-02-10 11:39:08 -0400

    Fix to the deploy

--- app/__init__.py
+++ app/__init__.py
@@ -14,7 +14,7 @@ login_manager = LoginManager()
 
 def create_app(config_name):
 
-     if os.getenv('FLASK_CONFIG') == "production":
+    if os.getenv('FLASK_CONFIG') == "production":
         app = Flask(__name__)
         app.config.update(
             SECRET_KEY=os.getenv('SECRET_KEY'),
